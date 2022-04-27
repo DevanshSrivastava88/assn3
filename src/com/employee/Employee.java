80125246bf5466e930e034cf21f3d14fb6414916
789253 <devanshxj@gmail.com>
2022-04-27 19:29:40 +0530

    hope it works

---
+++ src/com/employee/Employee.java
@@ -0,0 +1,78 @@
+package com.employee;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int salary;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	
+	public int getSalary() {
+		return salary;
+	}
+
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	
+	
+	
+	//-------------------
+	
+	String getName() {
+		return(this.firstName+this.lastName);
+	}
+	
+	int getAnnualSalary() {
+		return this.salary*12;
+	}
+	
+	int raiseSalary(int percent) {
+		return this.salary+ ((this.salary*percent)/100);
+	}
+
+	public Employee(int id, String firstName, String lastName, int salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ ", getId()=" + getId() + ", getFirstName()=" + getFirstName() + ", getLastName()=" + getLastName()
+				+ ", getSalary()=" + getSalary() + ", getName()=" + getName() + ", getAnnualSalary()="
+				+ getAnnualSalary() + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/com/employee/driver.java
@@ -0,0 +1,11 @@
+package com.employee;
+
+public class driver {
+ public static void main(String[] args) {
+	Employee e1=new Employee(1,"DSA","ASD",100);
+	System.out.println(e1.raiseSalary(5));
+	//System.out.println(e1);
+	
+
+}
+}
